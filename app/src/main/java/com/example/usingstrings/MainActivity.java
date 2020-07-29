0367506051988bdba02d1bdf2baad357b67af71d
Kethaka-Fonseka <kethaka.fsk@gmail.com>
2020-07-29 22:57:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView TMsg2=findViewById(R.id.tvMsg2);
+        TMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle","onCreate invoked");
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.i("Lifecycle", "onStart invoked");
+    }
+
+    public void onRestart(){
+        super.onRestart();
+        Log.i("Lifecycle","onRestart invoked");
+    }
+    public void onResume(){
+        super.onResume();
+        Log.i("Lifecycle","onResume invoked");
+    }
+    public void onPause(){
+        super.onPause();
+        Log.i("Lifecycle","onPause invoked");
+
+    }
+    public void onStop() {
+        super.onStop();
+        Log.i("Lifecycle", "onStop invoked");
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i("Lifecycle","onDestroy invoked");
+    }
+}
